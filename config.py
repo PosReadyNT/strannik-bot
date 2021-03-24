2d136855bea2ebb2d4785101c847cbd0ee67bdc5
PosReady <66481983+PosReadyNT@users.noreply.github.com>
2021-03-24 12:21:10 +0200

    обновление конфига

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ config = {
     "token": "your_token",
     "id": "your_ID",
     "name": "bot_name",
-    "version": "your_version_of_bot"
+    "version": "your_version_of_bot",
+    "mongo_db": "your_mongodb_key"
 }
